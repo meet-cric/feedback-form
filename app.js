1a9d24de0f87292273a8ac7f46639fed044f5096
meet_cric <mp32445@gmai.com>
2018-03-14 12:40:27 +0530

    [TASK:added routes and html]

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//setting up routes
 app.use('/', index);
 app.use('/users', users);
 
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("./feedback");
 });
-
+router.get('/submit',function(req,res,next){
+  res.render("./submit");
+})
 module.exports = router;
